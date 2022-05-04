d1572c5fae80b4196dce9856016b1928c8ae3f80
오동건 <odong-geon@odong-geon-ui-MacBookPro.local>
2022-05-04 20:25:31 +0900

    first git update

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+const path = require("path");
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.set("view engine", "pug");
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/assets")));
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT} SERVER START`)
+});
